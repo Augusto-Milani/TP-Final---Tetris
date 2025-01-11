595cc941fce439d7ee7383520bbf0b017fa43ff9
Augusto <amilani@itba.edu.ar>
2025-01-11 16:49:36 -0300

    Añado estadísticas, música y efectos de sonido

--- Tetris/front-pc.c
+++ Tetris/front-pc.c
@@ -9,6 +9,8 @@
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
 
 typedef struct {
 		ALLEGRO_EVENT event;			// Evento que ocurrió (ver switch).
@@ -19,10 +21,18 @@ typedef struct {
 		ALLEGRO_BITMAP *title;
 		ALLEGRO_BITMAP *background;
 		ALLEGRO_BITMAP *blocks;
+		ALLEGRO_SAMPLE* sfx4;
+		ALLEGRO_SAMPLE* sfx6;
+		ALLEGRO_SAMPLE* sfx8;
+		ALLEGRO_SAMPLE* music1;
+		ALLEGRO_SAMPLE_INSTANCE *sample_instance;
 		bool flagMenu;						// Bandera para menú principal y pausa.
 		bool flagPlay;						// Bandera para terminar programa.
 		bool redraw;					// Bandera para dibujar.
 	} argument_t;
+
+int score=40, top=1800, lines=13, level=2;
+int tetromino[7];
 
 static int TITLE_WIDTH, TITLE_HEIGHT, BACK_WIDTH, BACK_HEIGHT, TILE_WIDTH, TILE_HEIGHT;
 static int dx, dy, move;	//Centrar imagen en pantalla.
@@ -31,6 +41,7 @@ static argument_t argument;
 
 #define FILS 20
 #define COLS 10
+#define FONT_SIZE 26
 int matrix[FILS][COLS];
 
 static void check_init(void *pointer, const char *name);
@@ -68,6 +79,9 @@ int main() {
 	must_init(al_init_font_addon(), "font");
 	must_init(al_init_ttf_addon(), "ttf");
 	must_init(al_init_image_addon(), "image addon");
+	must_init(al_install_audio(), "audio");
+	must_init(al_init_acodec_addon(), "audio codecs");
+	must_init(al_reserve_samples(16), "reserve samples");
 
 
 
@@ -75,8 +89,33 @@ int main() {
 	ALLEGRO_DISPLAY *display = al_create_display(100, 100);
 	check_init(display, "display");
 
+	argument.music1 = al_load_sample("Music1.wav");
+	must_init(argument.music1, "music");
 
-	argument.font = al_load_font("nintendo-nes-font.ttf", 24, 0);
+	argument.sample_instance = al_create_sample_instance(argument.music1);
+	if ( !(argument.sample_instance) ) {
+		printf("Error: No se pudo crear la instancia del sample.\n");
+		al_destroy_sample(argument.music1);
+		return -1;
+	}
+
+	// Adjuntar la instancia al mezclador
+	al_attach_sample_instance_to_mixer(argument.sample_instance, al_get_default_mixer());
+
+	// Configurar el sample para que se reproduzca en bucle
+	al_set_sample_instance_playmode(argument.sample_instance, ALLEGRO_PLAYMODE_LOOP);
+
+
+	/* Efectos de Sonido */
+	argument.sfx4 = al_load_sample("SFX 4.wav");
+	must_init(argument.sfx4, "SFX 4");
+	argument.sfx6 = al_load_sample("SFX 6.wav");
+	must_init(argument.sfx6, "SFX 6");
+	argument.sfx8 = al_load_sample("SFX 8.wav");
+	must_init(argument.sfx8, "SFX 8");
+
+
+	argument.font = al_load_font("nintendo-nes-font.ttf", FONT_SIZE, 0);
 	check_init(argument.font, "tetris font TTF");
 
 	argument.background = al_load_bitmap(BACKGROUND_FILE);
@@ -86,15 +125,16 @@ int main() {
 	argument.blocks = al_load_bitmap(BLOCKS_FILE);
 	check_init(argument.blocks, "blocks image");
 
-	argument.timer = al_create_timer(1.0);
+	argument.timer = al_create_timer(1.0);	//Creo timer de 1 segundo
 	check_init(argument.timer, "timer");
 	argument.queue = al_create_event_queue();
 	check_init(argument.queue, "queue");
 
-
+	/* Cola de Eventos */
 	al_register_event_source(argument.queue, al_get_keyboard_event_source());
 	al_register_event_source(argument.queue, al_get_display_event_source(display));
 	al_register_event_source(argument.queue, al_get_mouse_event_source());
+	al_register_event_source(argument.queue, al_get_timer_event_source(argument.timer));
 
 	int SCREEN_WIDTH = al_get_display_width(display);
 	int SCREEN_HEIGHT = al_get_display_height(display);
@@ -135,6 +175,11 @@ int main() {
 	TetrisMenu();
 
 	/* Finalización del Programa */
+	al_destroy_sample_instance(argument.sample_instance);
+	al_destroy_sample(argument.sfx4);
+	al_destroy_sample(argument.sfx6);
+	al_destroy_sample(argument.sfx8);
+	al_destroy_sample(argument.music1);
 	al_destroy_font(argument.font);
 	al_destroy_display(display);
 	al_destroy_timer(argument.timer);
@@ -199,6 +244,9 @@ static void TetrisMenu() {
 
 static void TetrisPlay() {
 	playDraw();
+
+	al_play_sample_instance(argument.sample_instance);	//Reproduce música
+
 	argument.flagPlay = false;
 
 	while( !(argument.flagPlay) ) {
@@ -215,6 +263,7 @@ static void TetrisPlay() {
 				break;
 
 			case ALLEGRO_EVENT_MOUSE_BUTTON_DOWN:
+				al_play_sample(argument.sfx6, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_ONCE, NULL);
 				if((argument.event).mouse.button == 1) {	// Click izquierdo.
 					//TODO backend (pieza gira izq)***************
 				}
@@ -232,10 +281,12 @@ static void TetrisPlay() {
 
 				else {
 					if(al_key_down(&(argument.ks), ALLEGRO_KEY_LEFT)) {
+						al_play_sample(argument.sfx4, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_ONCE, NULL);
 						argument.redraw = true;
 						//TODO agregar backend acá*****************
 					}
 					else if(al_key_down(&(argument.ks), ALLEGRO_KEY_RIGHT)) {
+						al_play_sample(argument.sfx4, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_ONCE, NULL);
 						argument.redraw = true;
 						//TODO agregar backend acá*****************
 					}
@@ -261,6 +312,9 @@ static void TetrisPlay() {
 static void TetrisPause() {
 	pauseDraw(0);
 
+	unsigned int paused_position = al_get_sample_instance_position(argument.sample_instance);
+	al_stop_sample_instance(argument.sample_instance); // Detener la reproducción
+
 	int index = 0;
 	pauseDraw(index);
 
@@ -281,7 +335,10 @@ static void TetrisPause() {
 				al_get_keyboard_state(&(argument.ks));
 
 				if(al_key_down(&(argument.ks), ALLEGRO_KEY_ESCAPE)) {
+					al_set_sample_instance_position(argument.sample_instance, paused_position);	//Reanuda música
+					al_play_sample_instance(argument.sample_instance);
 					resume = true;
+					argument.redraw = true;
 				}
 				else if(al_key_down(&(argument.ks), ALLEGRO_KEY_UP)) {
 					argument.redraw = true;
@@ -296,10 +353,13 @@ static void TetrisPause() {
 					switch(index) {
 
 						case 0:	//CONTINUE
+							al_set_sample_instance_position(argument.sample_instance, paused_position);
+							al_play_sample_instance(argument.sample_instance);	//Reanuda música
 							argument.redraw = true;
 							break;					//Continúa el juego.
 
 						case 1:	//RESTART
+							al_play_sample_instance(argument.sample_instance);	//Reinicia música
 							argument.redraw = true;
 							//TODO backend acá?? ni idea	//Reinicia el juego
 							break;
@@ -310,7 +370,7 @@ static void TetrisPause() {
 							break;
 
 						case 3:	//CLOSE
-							argument.flagPlay = true;
+							argument.flagPlay = true;	//Cierra el programa.
 							argument.flagMenu = true;
 							break;
 
@@ -364,11 +424,36 @@ static void playDraw() {
 								 dx, dy, BACK_WIDTH * scale, BACK_HEIGHT * scale,
 								 0);
 	int i, j;
+	char str[7];	//TODO ver que no sea mayor al límite (%03, %06, %02)
+	sprintf(str, "%03d", lines);
+	al_draw_text(argument.font, al_map_rgb(255, 255, 255), dx + move*19, dy +move*2, 0, str);
+	sprintf(str, "%06d", top);
+	al_draw_text(argument.font, al_map_rgb(255, 255, 255), dx + move*24, dy +move*4, 0, str);
+	sprintf(str, "%06d", score);
+	al_draw_text(argument.font, al_map_rgb(255, 255, 255), dx + move*24, dy +move*7, 0, str);
+	sprintf(str, "%02d", level);
+	al_draw_text(argument.font, al_map_rgb(255, 255, 255), dx + move*26, dy +move*20, 0, str);
+
+	tetromino[0] = 2;
+	tetromino[3] = 19;
+	tetromino[5] = 24;
+	tetromino[6] = 300;
+	for(i=0 ; i<7 ; i++) {
+		sprintf(str, "%03d", tetromino[i]);
+		al_draw_text(argument.font, al_map_rgb(255, 255, 255), dx + move*6.5, dy +move*(11+i*2), 0, str);
+	}
+
+	for(i=0 ; i<4 ; i++) {
+		for(j=0 ; j<4 ; j++) {
+		al_draw_scaled_bitmap(argument.blocks,	0, 0,	TILE_WIDTH, TILE_HEIGHT,
+				dx + move*(24+j), dy + move*(13+i),	TILE_WIDTH * scale, TILE_HEIGHT * scale,	0);
+		}
+	}
 	for(i=0 ; i<FILS ; i++) {
 		for(j=0 ; j<COLS ; j++) {
 			if(matrix[i][j]) {
 				al_draw_scaled_bitmap(argument.blocks,	0, 0,	TILE_WIDTH, TILE_HEIGHT,
-									 dx + move*(12+j), dy + move*(5+i),	TILE_WIDTH * scale, TILE_HEIGHT * scale,		0);
+									 dx + move*(12+j), dy + move*(5+i),	TILE_WIDTH * scale, TILE_HEIGHT * scale,	0);
 			}
 		}
 	}
@@ -393,14 +478,14 @@ static void pauseDraw(int index) {
 
 
 /* NOTAS
-			 *
-			 * dx y dy indican la esquina izquierda del background, por lo que me desplazo
-			 * por cantidad de píxeles (26.4 aproximadamente) para cada bloque.
-			 *
-			 * Divido por 4.5 y 11 para ajustar con exactitud en base a la paleta de
-			 * bloques en el PNG usado.
-			 *
-			 * La escala se ajusta de manera global una vez al principio, TODO ver
-			 * con distintas resoluciones.
-			 *
-			 * */
+ *
+ * dx y dy indican la esquina izquierda del background, por lo que me desplazo
+ * por cantidad de píxeles (26.4 aproximadamente) para cada bloque.
+ *
+ * Divido por 4.5 y 11 para ajustar con exactitud en base a la paleta de
+ * bloques en el PNG usado.
+ *
+ * La escala se ajusta de manera global una vez al principio, TODO ver
+ * con distintas resoluciones.
+ *
+ * */
