23dde13a39ac5217388dc4fa14d274c49577ebf0
JuanSambu <jsambucetti@itba.edu.ar>
2025-02-01 16:02:35 -0300

    Bug minado parcheado, Bug nuevo descubierto

--- Tetris/backend.c
+++ Tetris/backend.c
@@ -16,107 +16,16 @@ void* pieces[PIECES_TETRIS] = {(void*)T, (void*)J, (void*)Z, (void*)O, (void*)S,
 //The order is not trivial, there's a complementary macro to this array that stores the size of each piece, to make the functions
 //accept more than a single size for matrix. We decided this was easier than passing around the size of each piece as an arg. 
 
-static int status[4][4];
+static int status[4][4];    //This matrix stores a copy of the piece that will spawn in board, so we can do the rotate logic in it instead
+                            //of using the ones defined in pieces.h
 int nextPieceID;
 
-//initialize the board, 20*10 size
-void initBoard() {
-	int i, j;
-    for (i = 0; i < BOARD_HEIGHT; i++) {
-        for (j = 0; j < BOARD_WIDTH; j++) {
-            board[i][j] = 0;
-        }
-    }
-    leaderBoard("------", 0, 0, 1);
-    
-    
-}
 
-void nextPiece() {
-	x_coord = (int)BOARD_WIDTH/2 - 2;
-	y_coord = 0;
-
-    if(mod) { //mod is a variable used only for testing purposes, kinda like a cheat code. don't tell anyone! 
-        nextPieceID = 6;
-    }
-    else {
-        nextPieceID = rand() % PIECES_TETRIS;	 //generates a random number between 0 and 1 less than the defined pieces
-    }
-    addPiece(nextPieceID);
-
-    mod = 0;	//TODO que hace???
-    //printf("Score: %d\n", score);
-}
+//**********************************************
+//*           DEBUGGING FUNCTIONS              *
+//**********************************************
 
 
-
-// Rotate function
-void rotateClockwise() {
-    int i, j, aux;
-    aux = PIECE_SIZE(nextPieceID);
-    
-    // OLD Prevent rotation if there's an obstacle
-    /*for (i = 0; i < aux; i++) {
-        for (j = 0; j < aux; j++) {
-
-            //Testing
-            printf("Coords being checked are (%d; %d)\n", (j + x_coord), (i + y_coord));
-            if((board[i + y_coord][j + x_coord] == 1)) printf("First statement holds true\n");
-            if(board[aux - 1 - j + x_coord][i + y_coord] == 2) printf("Second statement holds true\n");
-            //End testing
-
-        	if((board[i + y_coord][j + x_coord] == 1) && (board[aux - 1 - (j + x_coord)][i + y_coord] == 2)) {
-				//DO NOT touch, took me an hour to get those statements right.
-				//for every active cell in play, check if the coordinate where it should land is already filled by an old piece.
-				//if so, you can't rotate here.
-
-        		printf("got here!\n");  //this is a testament to how desperate i got. 
-				return;
-        	}
-        }
-    }*/
-
-
-//Prevent rotation if there's an obstacle (math before was wrong but i'm too attached to that code to delete it)
-for (i = y_coord; i < y_coord + aux; i++) {
-    for (j = x_coord; j < x_coord + aux; j++) {
-        if(board[i][j] == 1) {
-            if(board[(aux - (j - x_coord) + y_coord)][(aux - 1 - (i-y_coord) + x_coord)] > 1) {
-                return;
-            }
-        }
-    }
-}
-
-	//prevent the piece from rotating if any of its solid blocks will end up outside the boundaries
-	for (i = y_coord; i < y_coord + aux; i++) {
-        for (j = x_coord; j < x_coord + aux; j++) {
-			if (board[i][j] == 1 &&  ((aux - 1 - (i-y_coord) + x_coord) < 0 || (aux - 1 - (i-y_coord) + x_coord) >= BOARD_WIDTH)) { 
-				/* This statement deserves an explanation. for every moving block, check IF said block would fall outside the
-				boundaries of the board if it were rotated. (i-y_coord) gets the position of the block relative to the top left
-				corner. (aux - 1 - (i-y_coord)) is where the piece should land in the X axis, relative to the top left corner.
-				by adding x_coord, we convert to absolute coordinates. With that done, we can directly check if this value falls
-				outside of the allowed range of [0; BOARD_WIDTH).*/
-				return;
-			}
-		}
-	}
-
-    // Rotates
-    for (i = 0; i < aux; i++) {
-    	for (j = 0; j < aux; j++) {
-    		board[i + y_coord][j + x_coord] = status[aux-1-j][i];	//Rotates any matrix of ints, clockwise
-
-    	}
-    }
-    for (i = 0; i < aux; i++) {
-		for (j = 0; j < aux; j++) {
-			if(board[i + y_coord][j + x_coord] < 2) {
-				status[i][j] = board[i + y_coord][j + x_coord];		//Saves the status, except the static ones.
-			}
-		}
-    }
-}
 
 void printMatrix_3by3(int matrix[][3]) { //all 3's in this code are because we're working with a fixed size of 3.
 	int i, j;
@@ -151,6 +60,39 @@ void printBoard(){ //this is a stub
     printf("\n");
 }
 
+//**********************************************
+//*        GAME MECHANICS FUNCTIONS            *
+//**********************************************
+
+//initialize the board, 20*10 size
+void initBoard() {
+	int i, j;
+    for (i = 0; i < BOARD_HEIGHT; i++) {
+        for (j = 0; j < BOARD_WIDTH; j++) {
+            board[i][j] = 0;
+        }
+    }
+    leaderBoard("------", 0, 0, 1);
+    
+    
+}
+
+void nextPiece() {
+	x_coord = (int)BOARD_WIDTH/2 - 2;
+	y_coord = 0;
+
+    if(mod) { //mod is a variable used only for testing purposes, kinda like a cheat code. don't tell anyone! 
+        nextPieceID = 6;
+    }
+    else {
+        nextPieceID = rand() % PIECES_TETRIS;	 //generates a random number between 0 and 1 less than the defined pieces
+    }
+    addPiece(nextPieceID);
+
+    mod = 0;	//TODO que hace???
+    //printf("Score: %d\n", score);
+}
+
 
 /*void addPiece(int board[20][10], int nextPieceID) {
     if (nextPieceID == 0) { //ID 0 means the piece is an L
@@ -182,7 +124,7 @@ void addPiece(int nextPieceID) {
 			status[i][j] = 0;
 			if(piece[i][j] == 1) {
 				status[i][j] = 1;
-				if(board[i][j + x_coord] > 1) {
+				if(board[i][j + x_coord] > 1) { //if the board has a static piece where the game is trying to put a new piece, 
 					gameOver();
                     return;
 				}
@@ -195,6 +137,188 @@ void addPiece(int nextPieceID) {
 	}
 }
 
+
+//this function handles all the logic needed before every cycle starts with a new piece. 
+//it's called when, you guessed it, a piece collides with something.
+void collision() {
+
+	int i, j, k;
+
+    //Cell upgrading loop
+
+    for (i = 0; i < BOARD_HEIGHT; i++) {        //check the whole matrix for 1's and upgrade them to 2's every cycle.
+        for (j = 0; j < BOARD_WIDTH; j++) {
+            if(board[i][j] == 1) {
+				board[i][j] += nextPieceID+1;		// Allows to differentiate the pieces, for colouring in front-pc.
+			}
+        }
+    }
+
+    //Lane clearing logic
+
+    int currentLines, blocksPerRow;
+    for(i = BOARD_HEIGHT - 1, currentLines = 0; i >= 0; i--) {
+        for(j = 0, blocksPerRow = 0; j < BOARD_WIDTH; j++) { //check all the board for complete rows. Not the most efficient, but the safest.
+            
+            if(board[i][j] > 1) {
+                blocksPerRow++;
+            }
+        }
+        if(blocksPerRow == BOARD_WIDTH) { //check if we have a whole row. if so,
+            currentLines++; //increase the line counter for this event (used later for points counting)
+            
+            for(k = i; k > 0; k--) {
+                for(j = 0; j < BOARD_WIDTH; j++) {  //run upwards through every row of the board, starting from the full row,
+                    
+                    board[k][j] = board[k-1][j];    //and copy the line above into the current line.
+                    
+                }
+            }
+            for(j = 0; j < BOARD_WIDTH; j++) {
+                board[0][j] = 0; //we excluded row 0 before so as not to read nonexistent rows, now we clear row 0 separately.
+            }
+        i++;
+        }
+    }
+    
+    //Point math for lane clearing
+    switch(currentLines) {
+        case 1:
+            score += (40 * (level+1));
+            break;
+        case 2:
+            score += (100 * (level+1));
+            break;
+        case 3:
+            score += (300 * (level+1));
+            break;
+        case 4:
+            score += (1200 * (level+1));
+            break;
+        default:
+            break;
+    }
+	
+    lines += currentLines;
+    level = (lines / 10);
+	
+
+    tetromino[nextPieceID]++;
+	//score++;
+	nextPiece(mod);
+}
+
+
+void gameOver() {
+    printf("Womp womp\nYour score was %d\n", score);
+    printf("level reached: %d\n", lines);
+    
+}
+
+void leaderBoard(char inputName[6], unsigned long int inputScore, unsigned int inputLevel, int firstGame) {
+    if(firstGame) {
+        struct player {
+        char name[6];
+        unsigned long int score;
+        unsigned int level;
+        };
+        struct player leaderBoard[3];
+    }
+    
+}
+
+
+
+
+//**********************************************
+//*            MOVEMENT FUNCTIONS              *
+//**********************************************
+
+
+// Rotate function
+void rotateClockwise() {
+    int i, j, aux;
+    aux = PIECE_SIZE(nextPieceID);
+    
+    // OLD Prevent rotation if there's an obstacle
+    /*for (i = 0; i < aux; i++) {
+        for (j = 0; j < aux; j++) {
+
+            //Testing
+            printf("Coords being checked are (%d; %d)\n", (j + x_coord), (i + y_coord));
+            if((board[i + y_coord][j + x_coord] == 1)) printf("First statement holds true\n");
+            if(board[aux - 1 - j + x_coord][i + y_coord] == 2) printf("Second statement holds true\n");
+            //End testing
+
+        	if((board[i + y_coord][j + x_coord] == 1) && (board[aux - 1 - (j + x_coord)][i + y_coord] == 2)) {
+				//DO NOT touch, took me an hour to get those statements right.
+				//for every active cell in play, check if the coordinate where it should land is already filled by an old piece.
+				//if so, you can't rotate here.
+
+        		printf("got here!\n");  //this is a testament to how desperate i got. 
+				return;
+        	}
+        }
+    }*/
+
+
+//Prevent rotation if there's an obstacle (math before was wrong but i'm too attached to that code to delete it)
+for (i = y_coord; i < y_coord + aux; i++) {
+    for (j = x_coord; j < x_coord + aux; j++) {
+        if(board[i][j] == 1) {
+            if(board[(aux - (j - x_coord) + y_coord)][(aux - 1 - (i-y_coord) + x_coord)] > 1) {
+                //for every active cell in play, check if the coordinate where it should land is already filled by an old piece.
+				//if so, you can't rotate here. The double if instead of an && is only here for readability
+                return;
+            }
+        }
+    }
+}
+
+	//prevent the piece from rotating if any of its solid blocks will end up outside the boundaries
+	for (i = y_coord; i < y_coord + aux; i++) {
+        for (j = x_coord; j < x_coord + aux; j++) {
+			if (board[i][j] == 1 &&  ((aux - 1 - (i-y_coord) + x_coord) < 0 || (aux - 1 - (i-y_coord) + x_coord) >= BOARD_WIDTH)) { 
+				/* This statement deserves an explanation. for every moving block, check IF said block would fall outside the
+				boundaries of the board if it were rotated. (i-y_coord) gets the position of the block relative to the top left
+				corner. (aux - 1 - (i-y_coord)) is where the piece should land in the X axis, relative to the top left corner.
+				by adding x_coord, we convert to absolute coordinates. With that done, we can directly check if this value falls
+				outside of the allowed range of [0; BOARD_WIDTH).*/
+				return;
+			}
+		}
+	}
+
+    // Rotates
+    //alternativeRotation();
+    for (i = 0; i < aux; i++) {
+    	for (j = 0; j < aux; j++) {
+    		if (board[i + y_coord][j + x_coord] <= 1) {    
+                board[i + y_coord][j + x_coord] = status[aux-1-j][i];	//Rotates any matrix of ints, clockwise
+            }
+    	}
+    }
+    for (i = 0; i < aux; i++) {
+		for (j = 0; j < aux; j++) {
+			if(board[i + y_coord][j + x_coord] < 2) {
+				status[i][j] = board[i + y_coord][j + x_coord];		//Saves the status, except the static ones.
+			}
+		}
+    }
+}
+
+/*alternativeRotation() {
+    int aux = PIECE_SIZE(nextPieceID);
+    for(int i = y_coord; i < y_coord + aux; i++) {
+        for (int j = x_coord; j < x_coord + aux; j++) {
+            if(board[i][j] == 1) {
+                
+                board[(aux - (j - x_coord) + y_coord)][(aux - 1 - (i-y_coord) + x_coord)] = 1;
+                board[i][j] = 0;
+            }
+        }
+    }
+}*/
 
 
 int shiftPieceDown(int keyPressed) {	//Returns 1 if there's collision, 0 if not. (to display sound)
@@ -297,93 +421,4 @@ void shiftPieceLeft() {
     }
 
     x_coord--;
-}
-
-//this function handles all the logic needed before every cycle starts with a new piece. 
-//it's called when, you guessed it, a piece collides with something.
-void collision() {
-
-	int i, j, k;
-
-    //Cell upgrading loop
-
-    for (i = 0; i < BOARD_HEIGHT; i++) {        //check the whole matrix for 1's and upgrade them to 2's every cycle.
-        for (j = 0; j < BOARD_WIDTH; j++) {
-            if(board[i][j] == 1) {
-				board[i][j] += nextPieceID+1;		// Allows to differentiate the pieces, for colouring in front-pc.
-			}
-        }
-    }
-
-    //Lane clearing logic
-
-    int currentLines, blocksPerRow;
-    for(i = BOARD_HEIGHT - 1, currentLines = 0; i >= 0; i--) {
-        for(j = 0, blocksPerRow = 0; j < BOARD_WIDTH; j++) { //check all the board for complete rows. Not the most efficient, but the safest.
-            
-            if(board[i][j] > 1) {
-                blocksPerRow++;
-            }
-        }
-        if(blocksPerRow == BOARD_WIDTH) { //check if we have a whole row. if so,
-            currentLines++; //increase the line counter for this event (used later for points counting)
-            
-            for(k = i; k > 0; k--) {
-                for(j = 0; j < BOARD_WIDTH; j++) {  //run upwards through every row of the board, starting from the full row,
-                    
-                    board[k][j] = board[k-1][j];    //and copy the line above into the current line.
-                    
-                }
-            }
-            for(j = 0; j < BOARD_WIDTH; j++) {
-                board[0][j] = 0; //we excluded row 0 before so as not to read nonexistent rows, now we clear row 0 separately.
-            }
-        i++;
-        }
-    }
-    
-    //Point math for lane clearing
-    switch(currentLines) {
-        case 1:
-            score += (40 * (level+1));
-            break;
-        case 2:
-            score += (100 * (level+1));
-            break;
-        case 3:
-            score += (300 * (level+1));
-            break;
-        case 4:
-            score += (1200 * (level+1));
-            break;
-        default:
-            break;
-    }
-	
-    lines += currentLines;
-    level = (lines / 10);
-	
-
-    tetromino[nextPieceID]++;
-	//score++;
-	nextPiece(mod);
-}
-
-
-void gameOver() {
-    printf("Womp womp\nYour score was %d\n", score);
-    printf("level reached: %d\n", lines);
-    
-}
-
-void leaderBoard(char inputName[6], unsigned long int inputScore, unsigned int inputLevel, int firstGame) {
-    if(firstGame) {
-        struct player {
-        char name[6];
-        unsigned long int score;
-        unsigned int level;
-        };
-        struct player leaderBoard[3];
-    }
-    
 }
--- Tetris/backend.h
+++ Tetris/backend.h
@@ -27,5 +27,6 @@ void collision();
 void initBoard();
 void gameOver();
 void leaderBoard(char[6], unsigned long int, unsigned int, int);
+//void alternativeRotation(void);
 
 #endif /* TETRIS_BACKEND_H_ */
